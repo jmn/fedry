a258d58aedec536da71ec602734e627b35a1d3c8
Fedry <fedry@jmnorlund.net>
2017-09-08 20:21:32 +0200

    prod commit

--- dj/settings.py
+++ dj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z41==&_v&t7zop!0ic6j8j#460iqwjbo50^v-s$6s-#_=i*zpg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
