23f990d09455670cb39820b132eaabb101286954
jmn <norlund@gmail.com>
2017-09-10 17:51:13 +0200

    show user's feedsources in admin interface (TabularInline)

--- sn/admin.py
+++ sn/admin.py
@@ -3,17 +3,14 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
 from sn.models import Muppet, Post
+from fd.models import FeedSource
 
-# Define an inline admin descriptor for Employee model
-# which acts a bit like a singleton
-class MuppetInline(admin.StackedInline):
-    model = Muppet
-    can_delete = False
-    verbose_name_plural = 'muppet'
+class FeedSourceInline(admin.TabularInline):
+    model = FeedSource
+    extra = 0 
 
-# Define a new User admin
 class UserAdmin(BaseUserAdmin):
-    inlines = (MuppetInline, )
+    inlines = (FeedSourceInline, )
 
 # Re-register UserAdmin
 admin.site.unregister(User)
