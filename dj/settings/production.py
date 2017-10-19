1b1ba18f3f11b125d5a13f2754ef5bb0e33eb406
jmn <norlund@gmail.com>
2017-10-19 18:56:14 +0200

    trying to fix staticfiles

--- dj/settings/production.py
+++ dj/settings/production.py
@@ -115,6 +115,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ('/home/fedry/dj/static', BASE_DIR)
+STATIC_ROOT = '/home/fedry/static'
 #STATICFILES_DIRS = (                                                                 
 #  os.path.join(BASE_DIR, 'static/'),                                                
 #  BASE_DIR                                                                          
