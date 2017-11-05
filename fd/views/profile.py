6fd069d2ada3ecdac1947ebb4900b9a650b59310
jmn <norlund@gmail.com>
2017-11-05 23:37:54 +0100

    subs

--- fd/views/profile.py
+++ fd/views/profile.py
@@ -5,7 +5,26 @@ import stripe
 import djstripe
 #from pinax.stripe.actions import customers, subscriptions
 
+
+@csrf_exempt
 def get_user_profile(request):
+    if request.method == "POST":
+        stripe.api_key = djstripe.settings.STRIPE_SECRET_KEY
+        token = request.POST['stripeToken']
+        email = request.POST['stripeEmail']
+        
+        try:
+            charge = stripe.Customer.create(
+                email=email,
+                source=token,
+                plan='fedry'
+            )
+            messages.info(request, "Payment is successful")
+        except stripe.error.CardError as e:
+            # The card has been declined
+            print(e.message)
+            pass
+    
 #    user = request.user
 #    customer = customers.get_customer_for_user(request.user)
 #    sub = subscriptions.retrieve(customer, '')
