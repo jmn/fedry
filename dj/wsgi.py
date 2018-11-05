19fdb835ab6cdec2d4315aa3d2aa6dda01d128fa
jmn <norlund@gmail.com>
2018-11-05 19:06:40 +0100

    django-social-auth latest

--- dj/wsgi.py
+++ dj/wsgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dj.settings")
-os.environ.setdefault('DJANGO_CONFIGURATION', 'production')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dj.settings.production")
 
 application = get_wsgi_application()
