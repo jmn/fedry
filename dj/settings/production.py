30472e6de2b341aff0fb9817396395edddb09678
jmn <norlund@gmail.com>
2018-11-06 15:04:03 +0100

    rq worker

--- dj/settings/production.py
+++ dj/settings/production.py
@@ -172,15 +172,15 @@ BOOTSTRAP4 = {
 
 RQ_QUEUES = {
     'default': {
-        'URL': os.getenv('REDISTOGO_URL', 'redis://localhost:6379/0'),
+        'URL': os.getenv('REDIS_URL', 'redis://localhost:6379/0'),
         'DEFAULT_TIMEOUT': 360,
     },
     'high': {
-        'URL': os.getenv('REDISTOGO_URL', 'redis://localhost:6379/0'),
+        'URL': os.getenv('REDIS_URL', 'redis://localhost:6379/0'),
         'DEFAULT_TIMEOUT': 500,
     },
     'low': {
-        'URL': os.getenv('REDISTOGO_URL', 'redis://localhost:6379/0'),
+        'URL': os.getenv('REDIS_URL', 'redis://localhost:6379/0'),
     }
 }
 
