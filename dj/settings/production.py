9eeea18322c9e2eb631425184ea3164eca8878f8
jmn <norlund@gmail.com>
2018-11-06 14:45:20 +0100

    rq worker

--- dj/settings/production.py
+++ dj/settings/production.py
@@ -90,9 +90,12 @@ WSGI_APPLICATION = 'dj.wsgi.application'
 #         'PORT': '',
 #     }
 # }
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
