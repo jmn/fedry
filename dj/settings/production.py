56486cb2f5968491b00c3b84a956d86372fd7947
jmn <norlund@gmail.com>
2018-11-06 16:32:41 +0100

    redis rq herokuworker

--- dj/settings/production.py
+++ dj/settings/production.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
