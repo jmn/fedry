af638b1c2f21955d76a2326ce7b4953fccb5a2b1
jmn <norlund@gmail.com>
2018-11-20 22:59:44 +0100

    first in graphql now works

--- dj/schema.py
+++ dj/schema.py
@@ -1,16 +1,21 @@
 # post
 from graphene_django import DjangoObjectType
+from graphene_django.filter.fields import DjangoFilterConnectionField
+
 from fd.models import FeedPost
 import graphene
 
 class Post(DjangoObjectType):
     class Meta:
         model = FeedPost
-
+        interfaces = (graphene.Node,)
+        filter_fields = []
+        
 class Query(graphene.ObjectType):
-    posts = graphene.List(Post)
+#    posts = graphene.List(Post)
+    posts = DjangoFilterConnectionField(Post)    
 
-    def resolve_posts(self, info):
+    def resolve_posts(self, info, **args):
         return FeedPost.objects.all()
 
 schema = graphene.Schema(query=Query)
