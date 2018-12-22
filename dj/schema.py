ad30afa8794a71db90116cf1c1d06356599586f1
jmn <norlund@gmail.com>
2018-12-22 21:58:21 +0100

    update schema to support search

--- dj/schema.py
+++ dj/schema.py
@@ -4,7 +4,7 @@ from graphene_django.filter.fields import DjangoFilterConnectionField
 from django.contrib.auth.models import User
 from fd.models import FeedPost, FeedSource
 from django.db import models
-from django.db.models import F
+from django.db.models import F, Q
 import graphene
 
 class PostType(DjangoObjectType):
@@ -14,10 +14,10 @@ class PostType(DjangoObjectType):
         model = FeedPost
         interfaces = (graphene.relay.Node,)
         filter_fields = ['feed']
-        
+
 class Query(graphene.ObjectType):
     post = graphene.Node.Field(PostType, username = graphene.String())
-    all_posts = DjangoFilterConnectionField(PostType, username=graphene.String(required=True))
+    all_posts = DjangoFilterConnectionField(PostType, username=graphene.String(required=True), q=graphene.String())
     reverse = graphene.String(word=graphene.String())
 
     def resolve_reverse(self, info, word):
@@ -26,10 +26,15 @@ class Query(graphene.ObjectType):
     # https://stackoverflow.com/a/39774434/41829
     def resolve_all_posts(self, info, **kwargs):
         username = kwargs.get('username')
-        u = User.objects.get(username=username) 
+        u = User.objects.get(username=username)
+        searchterm = kwargs.get('q')
         users_sources = FeedSource.objects.filter(user=u, show_on_frontpage=True)
-        return FeedPost.objects.filter(feed__feedsource__in=users_sources).annotate(
-            source_title=F('feed__feedsource__title')
-        )
+
+        if searchterm:
+            queryset = FeedPost.objects.filter(feed__feedsource__in=users_sources).filter(Q(title__icontains=searchterm) | Q(content__icontains=searchterm)).annotate(source_title=F('feed__feedsource__title'))
+        else:
+            queryset = FeedPost.objects.filter(feed__feedsource__in=users_sources).annotate(
+                source_title=F('feed__feedsource__title'))
+        return queryset
         
 schema = graphene.Schema(query=Query)
